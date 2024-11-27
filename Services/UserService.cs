df9cd62a504923c034eca8f59be7e7d18fd32cb2
MagcalasNorman <magcalas.normanmiguel@auf.edu.ph>
2024-11-27 09:21:50 +0800

    Add project files.

---
+++ Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module08.Model;
+using System.Net.Http.Json;
+
+namespace Module08.Services
+{
+     public class UserService
+    {
+        private readonly HttpClient _httpClient;
+        private const string BaseUrl = "http://localhost/pdc50";
+
+        public UserService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<User>>GetUserAsync()
+            {
+            var response = await _httpClient.GetFromJsonAsync <List<User>>($"{BaseUrl}get_user.php");
+            return response ?? new List<User> ();
+            }
+    }
+}
---
+++ ViewModel/UserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module08.Model;
+using Module08.Services;
+using System.Windows.Input;
+
+namespace Module08.ViewModel
+{
+    public class UserViewModel : BindableObject
+    {
+        private readonly UserService _userService;
+        public ObservableCollection<User> Users { get; set; }
+
+        public UserViewModel()
+        {
+            _userService = new UserService();
+            Users = new ObservableCollection<User>();
+            LoadUserCommand = new Command(async () => await LoadUsers());
+        }
+
+        public ICommand LoadUserCommand { get; }
+        private async Task LoadUsers()
+        {
+            var users = await _userService.GetUserAsync();
+            Users.Clear();
+            foreach (var user in users)
+            {
+                Users.Add(user);
+            }
+
+        }
+    }
+}
